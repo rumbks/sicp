358893677e8da64f46fbb2294258d512f6a81fe1
Igor_Aukhadeev <rumbks@gmail.com>
2023-03-05 13:56:28 +0500

    Table representation

--- sicp/subchapter3_2.rkt
+++ sicp/subchapter3_2.rkt
@@ -63,22 +63,22 @@
 (define m `(a b))
 (define e2 (cons m (cdr m)))
 ;(count-pairs e2) 
-(check-cycle e1)
-(check-cycle e2)
+;(check-cycle e1)
+;(check-cycle e2)
 
 (define e3 (cons `a `()))
-(set-cdr! e3 e3)
-(check-cycle e3)
+;(set-cdr! e3 e3)
+;(check-cycle e3)
 
 (define e4 (list `a `b `c))
 
-(cddr e4)
-(set-cdr! (cddr e4) e4)
-(check-cycle e4)
+;(cddr e4)
+;(set-cdr! (cddr e4) e4)
+;(check-cycle e4)
 
 (define e5 (list `a `b `c))
-(set-cdr! (cddr e5) (cdr e5))
-(check-cycle e5)
+;(set-cdr! (cddr e5) (cdr e5))
+;(check-cycle e5)
 
 (define (make-queue) 
   (let 
@@ -109,9 +109,94 @@
             ((eq? m `delete!) delete-queue!))) 
     dispatch))
 
-(define q (make-queue))
-((q `insert!) `a)
-((q `insert!) `b)
-(q `front)
-((q `delete!))
-(q `front)
+;(define q (make-queue))
+;((q `insert!) `a)
+;((q `insert!) `b)
+;(q `front)
+;((q `delete!))
+;(q `front)
+
+(define (make-table same-key?)
+
+  (let ((local-table (list `*table*)))
+
+    (define (lookup key)
+      (let ((record (assoc key (cdr local-table))))
+        (if record
+          (cdr record)
+          false)))
+
+    (define (assoc key records)
+      (cond ((null? records) false)
+            ((same-key? key (caar records)) (car records))
+            (else (assoc key (cdr records)))))
+
+    (define (insert! key value)
+      (let ((record (assoc key (cdr local-table))))
+        (if record
+          (set-cdr! record value)
+          (set-cdr! local-table (cons (cons key value) (cdr local-table)))))
+      `ok)
+
+    (define (dispatch m)
+      (cond ((eq? m `lookup) lookup)
+            ((eq? m `insert!) insert!)
+            (else (error "Unknown operation"))))
+    dispatch))
+
+;(define table (make-table equal?))
+;(define get (table `lookup))
+;(define put (table `insert!))
+
+;(get `key-1)
+;(put `Maruf 25)
+;(put `Igor 24)
+;(get `Maruf)
+;(get `Igor)
+
+
+(define (make-table same-key?)
+
+  (let ((local-table (list `*table*)))
+
+    (define (lookup . keys)
+      (define (iter keys table)
+        (if (eq? keys `())
+          (cdr table) 
+          (let ((subtable (assoc (car keys) (cdr table))))
+            (if subtable
+              (iter (car keys) subtable)
+              false))))
+      (iter keys local-table))
+
+    (define (assoc key records)
+      (cond ((null? records) false)
+            ((same-key? key (caar records)) (car records))
+            (else (assoc key (cdr records)))))
+
+    (define (make-insert-object keys value) 
+      (define (iter keys result) 
+        ; (let ((reversed-keys (reverse keys)))...
+        (if (eq? keys `()) result 
+          (iter (cdr keys) (cons (cons ...))))))
+    ; проходимся по ключам в обратном порядке, строим табличный объект
+    (define (insert! value . keys)
+      (define (iter keys)
+        ; переходим по ключам в подтаблицы до тех пор, пока получается, при этом храним 
+        ; ссылку на текущую таблицу
+        ;В тот момент, когда  не получается, строим объект и вставляем его в
+        ; текущую подтаблицу
+        ()))
+
+    (define (dispatch m)
+      (cond ((eq? m `lookup) lookup)
+            ((eq? m `insert!) insert!)
+            (else (error "Unknown operation"))))
+    dispatch))
+
+
+
+
+
+
+
